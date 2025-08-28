ba9e5fd90512dff6d8bcbde56474e114992b180b
ZXPrism <2897544036@qq.com>
2025-08-28 22:17:12 +0800

    finish

--- include/gfx-utils-core/render_pass.h
+++ include/gfx-utils-core/render_pass.h
@@ -10,26 +10,34 @@
 #include <vector>
 
 #include <glad/glad.h>
+#include <glm/glm.hpp>
 
 namespace gfxutils {
 
 class RenderPass : public IBuildTarget<RenderPass> {
 private:
+	bool _IsDefault;  // i.e. use default FBO
+
 	std::shared_ptr<GLuint> _FBO;
 	std::vector<Texture> _ColorAttachments;
-	Texture _DepthAttachment;
+	std::vector<bool> _ColorAttachmentClearFlags;
+	std::vector<glm::vec4> _ColorAttachmentClearValues;
+	std::optional<Texture> _DepthAttachment;
 
 public:
 	class RenderPassBuilder : public IBuilder<RenderPassBuilder, RenderPass> {
 	private:
 		std::vector<Texture> _ColorAttachments;
-		std::vector<bool> _ColorAttachmentClearOptions;
+		std::vector<bool> _ColorAttachmentClearFlags;
+		std::vector<glm::vec4> _ColorAttachmentClearValues;
 		std::optional<Texture> _DepthAttachment;
 
 	public:
 		RenderPassBuilder(const std::string &name);
 
-		RenderPassBuilder &add_color_attachment(const Texture &texture, bool clear_before_use);  // TODO: control clear colors for each attachments...
+		// NOTE: current only support float types
+		// NOTE: if attachment's one component has less than 4 floats, only clear_value_rgba.r is used to clear it
+		RenderPassBuilder &add_color_attachment(const Texture &texture, bool clear_before_use, const glm::vec4 &clear_value_rgba = {});
 		RenderPassBuilder &set_depth_attachment(const Texture &texture);
 
 		RenderPass _build();
