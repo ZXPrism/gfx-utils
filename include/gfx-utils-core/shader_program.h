1da1b032745561cabde6a998d0881c161a686af6
ZXPrism <2897544036@qq.com>
2025-08-29 15:50:45 +0800

    refactor: isolate shader class

--- include/gfx-utils-core/shader_program.h
+++ include/gfx-utils-core/shader_program.h
@@ -3,40 +3,17 @@
 #include <memory>
 #include <string>
 #include <unordered_map>
-#include <unordered_set>
 #include <vector>
 
 #include <gfx-utils-core/interfaces/build_target.h>
 #include <gfx-utils-core/interfaces/builder.h>
+#include <gfx-utils-core/shader.h>
 #include <gfx-utils-core/shader_types.h>
 
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 
 namespace gfxutils {
-
-class Shader : public IBuildTarget<Shader> {
-private:
-	std::shared_ptr<GLuint> _ShaderHandle;
-
-public:
-	class ShaderBuilder : public IBuilder<ShaderBuilder, Shader> {
-	private:
-		ShaderType _ShaderType = ShaderType::UNKNOWN;
-		std::string _Source;
-
-	public:
-		ShaderBuilder(const std::string &name);
-
-		ShaderBuilder &set_type(ShaderType shader_type);
-		ShaderBuilder &set_source(const std::string &shader_source);
-		ShaderBuilder &set_source_from_file(const std::string &source_file_path);
-
-		Shader _build() const;
-	};
-
-	GLuint _get_handle() const;
-};
 
 class ShaderProgram : public IBuildTarget<ShaderProgram> {
 private:
---
+++ src/shader.cpp
@@ -0,0 +1,88 @@
+#include <gfx-utils-core/shader.h>
+
+#include <gfx-utils-core/logger.h>
+
+#include <fstream>
+#include <sstream>
+
+namespace gfxutils {
+
+Shader::ShaderBuilder::ShaderBuilder(const std::string &name)
+    : IBuilder(name) {
+}
+
+Shader::ShaderBuilder &Shader::ShaderBuilder::set_type(ShaderType shader_type) {
+	_ShaderType = shader_type;
+	return *this;
+}
+
+Shader::ShaderBuilder &Shader::ShaderBuilder::set_source(const std::string &shader_source) {
+	_Source = shader_source;
+	return *this;
+}
+
+Shader::ShaderBuilder &Shader::ShaderBuilder::set_source_from_file(const std::string &source_file_path) {
+	std::ifstream fin(source_file_path);
+	if (!fin) {
+		g_logger->warn("Shader::ShaderBuilder ({}): failed to load shader source file from {}", _Name, source_file_path);
+		return *this;
+	}
+
+	std::stringstream ssm;
+	ssm << fin.rdbuf();
+	_Source = ssm.str();
+	fin.close();
+
+	return *this;
+}
+
+Shader Shader::ShaderBuilder::_build() const {
+	Shader res;
+
+	if (_Source.empty()) {
+		return res;
+	}
+
+	GLuint *raw_shader_handle = new GLuint(0);
+	res._ShaderHandle = std::shared_ptr<GLuint>(raw_shader_handle, [](GLuint *ptr) {
+		glDeleteShader(*ptr);
+		delete ptr;
+	});
+
+	switch (_ShaderType) {
+	case ShaderType::VERTEX_SHADER:
+		*res._ShaderHandle = glCreateShader(GL_VERTEX_SHADER);
+		break;
+	case ShaderType::FRAGMENT_SHADER:
+		*res._ShaderHandle = glCreateShader(GL_FRAGMENT_SHADER);
+		break;
+	case ShaderType::COMPUTE_SHADER:
+		*res._ShaderHandle = glCreateShader(GL_COMPUTE_SHADER);
+		break;
+	default:
+		g_logger->warn("Shader::ShaderBuilder ({}): unknown shader type, renderer may not work correctly", _Name);
+		return res;
+	}
+
+	const char *const shader_src = _Source.c_str();
+	glShaderSource(*res._ShaderHandle, 1, &shader_src, nullptr);
+	glCompileShader(*res._ShaderHandle);
+
+	GLint compile_status;
+	glGetShaderiv(*res._ShaderHandle, GL_COMPILE_STATUS, &compile_status);
+	if (!compile_status) {
+		static char compile_log[1024];
+		glGetShaderInfoLog(*res._ShaderHandle, sizeof(compile_log), nullptr, compile_log);
+		g_logger->warn("Shader::ShaderBuilder ({}): shader compilation failed:\n{}", _Name, compile_log);
+	}
+
+	res._set_complete();
+
+	return res;
+}
+
+GLuint Shader::_get_handle() const {
+	return *_ShaderHandle;
+}
+
+}  // namespace gfxutils
